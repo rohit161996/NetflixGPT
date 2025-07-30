c80e8910ea286a29f2dca931dcd3cba237fc3767
rohitramchandanistm1996@gmail.com <Radha@1234567890->
2025-07-30 12:16:36 +0530

    Netflix GPT Project Added

---
+++ src/Components/MovieCard.jsx
@@ -0,0 +1,17 @@
+import { IMAGE_URL } from "../utils/constants";
+
+const MovieCard = ({posterPath}) => {
+
+    if(!posterPath) return null;
+
+    return (
+        <div className="w-48 pr-4">
+            <img
+                className="cursor-pointer"
+                src={IMAGE_URL + posterPath}
+                alt="MovieCard" />
+        </div>
+    )
+}
+
+export default MovieCard;
